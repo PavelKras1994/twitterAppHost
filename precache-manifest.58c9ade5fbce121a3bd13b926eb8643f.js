72b555d473a11d7ccc09d207605cc2d38b2712d1
Pavel <pavekrasnoschok@gmai.com>
2020-10-12 07:28:59 +0300

    first commit

---
+++ precache-manifest.58c9ade5fbce121a3bd13b926eb8643f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a00aeab955e71daa84d72b990a07ed85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6de793ac7248f621d56",
+    "url": "/static/css/main.a354b6d4.chunk.css"
+  },
+  {
+    "revision": "f194d5c690e81ca02871",
+    "url": "/static/js/2.604c8791.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.604c8791.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6de793ac7248f621d56",
+    "url": "/static/js/main.cf455e2b.chunk.js"
+  },
+  {
+    "revision": "2443006efb8acf0ab422",
+    "url": "/static/js/runtime-main.4e02f2f3.js"
+  }
+]);
